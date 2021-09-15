cf760aedfbaed297e7a8c5f4f2ad62faffd34444
zeshuan <zhangzs000@hotmail.com>
2021-09-15 09:50:52 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'react/jsx-uses-react': 'off', // react 17 之后，可以关掉此配置
+    'react/react-in-jsx-scope': 'off' // react 17 之后，可以关掉此配置
+  },
+  settings: {
+    react: {
+      // 需要配置，否则报 'Warning: React version not specified in eslint-plugin-react settings.'
+      version: 'detect'
+    }
+  }
+}
